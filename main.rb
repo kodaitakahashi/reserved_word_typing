7e2efb1855e7480fc518d6357f8fd90c65df452c
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2016-08-31 12:52:29 +0900

    change main.rb  Modelをrequireで呼び出し、その先でActiveRecordでの接続をするようにした

--- main.rb
+++ main.rb
@@ -2,11 +2,7 @@ require 'sinatra/base'
 require 'sinatra/config_file'
 require 'slim'
 require 'active_record'
-
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'dev.db'
-)
+require './models/reserved_word'
 
 class Main < Sinatra::Base
   register Sinatra::ConfigFile
