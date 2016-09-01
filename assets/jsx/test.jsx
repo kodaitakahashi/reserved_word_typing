8ea3ae9b5ebcd2f87b439d0f807ec3e3fd36fd76
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2016-09-01 11:19:09 +0900

    add TypingWindow ajaxで受け取ったJSONを表示することはできた  バグ stateの初期値の設定により最初のajaxのURL先を間違える

--- assets/jsx/test.jsx
+++ assets/jsx/test.jsx
@@ -1,13 +1,65 @@
+var MainContents = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <Button initializeId="0"/>
+            </div>
+        );
+    }
+});
+
 var Button = React.createClass({
     getInitialState: function() {
         return{
             ruby: 1,
+            selectProgramId: this.props.initializeId
+        };
+    },
+    setProgramId: function(event) {
+        this.setState({selectProgramId: event.target.value});
+        console.log(this.state.selectProgramId);
+    },
+    /* selectQuesion: function(event) {
+     *     $.ajax({
+     *         url: "/quesions/" + event.target.value,
+     *         dataType: 'json',
+     *         cache: false,
+     *         success: function(data) {
+     *             this.setState({quesions: data});
+     *         }.bind(this),
+     *         error: function(xhr, status, error){
+     *             console.log("error");
+     *         }.bind(this)
+     *     });
+     * },*/
+    render: function() {
+        return (
+            <div>
+                <div id="buttons">
+                    <div className="pure-menu pure-menu-horizontal">
+                        <p className="pure-menu-heading">Reserved word typing</p>
+                        <ul className="pure-menu-list">
+                            <button className="pure-menu-item" onClick={this.setProgramId} value={this.state.ruby}>Ruby</button>
+                        </ul>
+                    </div>
+                </div>
+                <TypingWindow programId={this.state.selectProgramId} />
+            </div>
+        );
+    }
+});
+
+var TypingWindow = React.createClass({
+    getInitialState: function() {
+        return{
+            programId: this.props.programId,
             quesions: []
         };
     },
-    selectQuesion: function(event) {
+    componentWillReceiveProps: function() {
+        this.setState({programId: this.props.programId});
         $.ajax({
-            url: "/quesions/" + event.target.value,
+            url: "/quesions/" + this.state.programId,
             dataType: 'json',
             cache: false,
             success: function(data) {
@@ -19,19 +71,23 @@ var Button = React.createClass({
             }.bind(this)
         });
     },
-    render: function() {
-        return (
-            <div className="pure-menu pure-menu-horizontal">
-                <p className="pure-menu-heading">Reserved word typing</p>
-                <ul className="pure-menu-list">
-                    <button className="pure-menu-item" onClick={this.selectQuesion} value={this.state.ruby}>Ruby</button>
-                </ul>
+    render: function(){
+        var node = this.state.quesions.map(function(data, i){
+            return(
+                <div key={i} className="hoge">
+                    <p>{data.word}</p>
+                    <p>{data.description}</p>
+                </div>
+            );
+        });
+        return(
+            <div>
+                {node}
             </div>
         );
     }
 });
-
 ReactDOM.render(
-    <Button />,
-    document.getElementById('buttons')
+    <MainContents />,
+    document.getElementById('contents')
 );
